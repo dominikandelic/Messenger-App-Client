852c381688a5842148a54473f66b8b1d98c45dfc
dominikandelic <dominikandelic@localhost>
2022-02-13 22:46:17 +0100

    Initial commit

---
+++ src/main/java/hr/dandelic/tcpclient/Client.java
@@ -0,0 +1,100 @@
+package hr.dandelic.tcpclient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.net.Socket;
+import java.util.Scanner;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author dominikandelic
+ */
+public class Client {
+
+    private static PrintWriter out;
+    private static BufferedReader in;
+    private static Socket socket;
+    private static String userId;
+    private static String username;
+    private static Scanner scanner;
+    private static MessageReader messageReader;
+    private static ClientGui gui;
+    static String[] activeUserArray;
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        try {
+            SwingUtilities.invokeAndWait((gui = new ClientGui()));
+        } catch (InterruptedException | InvocationTargetException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        while (true) {
+            if (gui.getUsername() != null) {
+                username = gui.getUsername();
+                break;
+            }
+        }
+
+        client.start("localhost", 25555);
+        gui.setOut(out);
+        gui.addSubmitMessageListener();
+        Thread readerThread = new Thread(messageReader = new MessageReader(in, username, userId, gui));
+        readerThread.start();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                client.stop();
+                if (messageReader.isRunnable()) {
+                    messageReader.kill();
+                }
+            }
+        });
+    }
+
+    public void start(String host, int port) {
+        try {
+            socket = new Socket(host, port);
+            System.out.println("Successfully connected, sending greeting.");
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            out.printf("handshake\n");
+
+            userId = in.readLine();
+            System.out.println("ID: " + userId);
+            scanner = new Scanner(System.in);
+            out.println(username);
+
+            while (true) {
+                if (in.ready()) {
+                    String activeUserListString = in.readLine();
+                    activeUserArray = activeUserListString.split(",");
+                    gui.setActiveUserList();
+                    //System.out.println("Uhvatili smo userlist.");
+                    break;
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Failed to connect.");
+            System.exit(-1);
+
+        }
+    }
+
+    public void stop() {
+        try {
+            out.close();
+            in.close();
+            socket.close();
+            scanner.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/hr/dandelic/tcpclient/MessageReader.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hr.dandelic.tcpclient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author dominikandelic
+ */
+public class MessageReader implements Runnable {
+
+    BufferedReader in;
+    String line;
+    private boolean runnable = true;
+    private final ClientGui gui;
+
+    String username;
+    String userId;
+
+    public MessageReader(BufferedReader in, String username, String userId, ClientGui gui) {
+        this.in = in;
+        this.userId = userId;
+        this.username = username;
+        this.gui = gui;
+    }
+
+    public boolean isRunnable() {
+        return runnable;
+    }
+
+    public void kill() {
+        runnable = false;
+    }
+
+    @Override
+    public void run() {
+        while (runnable) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (in.ready()) {
+                    line = in.readLine();
+                    if (line.equals("QUIT_CLIENT")) {
+                        gui.getChatArea().append("Lost connection to the server.");
+                        gui.getMessageArea().setEditable(false);
+                        break;
+                    }
+                    if (line.contains("disconnected")) {
+                        String temp[] = line.split(" ");
+                        gui.getActiveUserList().removeElement(temp[0]);
+                    } else if (line.contains("connected")) {
+                        String temp[] = line.split(" ");
+                        gui.getActiveUserList().addElement(temp[0]);
+                    }
+                    gui.getChatArea().append(line + "\n");
+                }
+            } catch (IOException e) {
+                System.out.println("Thread stopped.");
+                break;
+            }
+        }
+        runnable = false;
+
+    }
+
+}
